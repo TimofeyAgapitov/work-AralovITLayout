9b393523e15d5cb232f82c9eabd7240641e22cb8
Timofey Agapitov <timonchikagapitov@gmail.com>
2023-10-30 15:37:05 +0300

    update

--- dist/js/app.js
+++ dist/js/app.js
@@ -25,4 +25,87 @@ $(document).ready(function () {
     closeAble: true,
     closeOther: false,
   });
+
+  // Убавляем кол-во по клику
+  $('.accordion-range-box .range-btn-minus').click(function () {
+    let $input = $(this).parent().find('.range-value');
+    let count = parseInt($input.val()) - 1;
+    count = count < 0 ? 0 : count;
+    $input.val(count);
+
+    // Управление классом _disactive для кнопки -
+    if (count === 0) {
+      $(this).addClass('_disactive');
+    } else {
+      $(this).removeClass('_disactive');
+    }
+
+    // Удаление класса _disactive с кнопки +
+    $(this).parent().find('.range-btn-plus').removeClass('_disactive');
+  });
+  // Прибавляем кол-во по клику
+  $('.accordion-range-box .range-btn-plus').click(function () {
+    let $input = $(this).parent().find('.range-value');
+    let count = parseInt($input.val()) + 1;
+    count =
+      count > parseInt($input.data('max-count'))
+        ? parseInt($input.data('max-count'))
+        : count;
+    $input.val(parseInt(count));
+
+    // Управление классом _disactive для кнопки +
+    if (count === 20) {
+      $(this).addClass('_disactive');
+    } else {
+      $(this).removeClass('_disactive');
+    }
+
+    // Удаление класса _disactive с кнопки -
+    $(this).parent().find('.range-btn-minus').removeClass('_disactive');
+  });
+  // Убираем все лишнее и невозможное при изменении поля
+  $('.accordion-range-box .range-value').bind(
+    'change keyup input click',
+    function () {
+      if (this.value.match(/[^0-9]/g)) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+      }
+      if (this.value == '') {
+        this.value = 1;
+      }
+      if (this.value > parseInt($(this).data('max-count'))) {
+        this.value = parseInt($(this).data('max-count'));
+      }
+    }
+  );
 });
+
+if (document.querySelector('.choose-tariffs')) {
+  const tariffsButtons = document.querySelectorAll(
+    '.choose-tariffs__btn .component--green-btn'
+  );
+  const tariffElements = document.querySelectorAll('.choose-tariffs__element');
+
+  tariffsButtons.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      // Удаляем класс "_active" у всех элементов
+      tariffElements.forEach((element) => {
+        element.classList.remove('_active');
+      });
+
+      // Изменяем текст всех кнопок на "Выбрать"
+      tariffsButtons.forEach((button) => {
+        button.querySelector('.green-btn__text').textContent = 'Выбрать';
+      });
+
+      // Добавляем класс "_active" только к элементу, на котором нажата кнопка
+      tariffElements[index].classList.add('_active');
+
+      // Изменяем текст выбранной кнопки на "Выбран"
+      const buttonText = btn.querySelector('.green-btn__text');
+      if (tariffElements[index].classList.contains('_active')) {
+        buttonText.textContent = 'Выбран';
+      }
+    });
+  });
+}
--- dist/js/swiper.js
+++ dist/js/swiper.js
@@ -1,3 +1,16 @@
+function remToPx(remValue) {
+  // Получаем текущий базовый размер шрифта (font-size) из элемента <html>
+  var htmlFontSize = parseFloat(
+    getComputedStyle(document.documentElement).fontSize
+  );
+
+  // Переводим значение из rem в px
+  var pxValue = remValue * htmlFontSize;
+
+  // Округляем значение до целых пикселей (по желанию)
+  return Math.round(pxValue) + 'px';
+}
+
 if (document.querySelector('.our-articles-swiper')) {
   new Swiper('.our-articles-swiper', {
     slidesPerView: 'auto',
@@ -28,6 +41,46 @@ if (document.querySelector('.our-teams-swiper')) {
         navigation: {
           nextEl: '.our-teams__controls .btn-next',
           prevEl: '.our-teams__controls .btn-prev',
+        },
+      },
+    },
+  });
+}
+
+if (document.querySelector('.our-letters-swiper')) {
+  new Swiper('.our-letters-swiper', {
+    slidesPerView: 'auto',
+    freemode: true,
+    speed: 1000,
+    breakpoints: {
+      769: {
+        slidesPerView: 'auto',
+        speed: 1000,
+        navigation: {
+          nextEl: '.our-letters__controls .btn-next',
+          prevEl: '.our-letters__controls .btn-prev',
+        },
+      },
+    },
+  });
+}
+
+if (document.querySelector('.grid-articles-swiper')) {
+  new Swiper('.grid-articles-swiper', {
+    slidesPerView: 'auto',
+    freemode: true,
+    speed: 1000,
+    breakpoints: {
+      769: {
+        slidesPerView: 3,
+        grid: {
+          rows: 2,
+        },
+        spaceBetween: remToPx(4),
+        speed: 1000,
+        navigation: {
+          nextEl: '.grid-articles__controls .btn-next',
+          prevEl: '.grid-articles__controls .btn-prev',
         },
       },
     },
